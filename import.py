18903f0656eff85659a5902d6470a40ceae0636f
SkoryninaKaterina <skorynina.katerina95@gmail.com>
2025-05-13 21:31:38 +0300

    gggnh

--- import.py
+++ import.py
@@ -1,1 +1,18 @@
-import cv2, torch
+import cv2
+
+# Завдання 3
+# Відкрийте зображення data/lesson1/Lenna.png
+# Створіть наступні зображення:
+
+img = cv2.imread("data/lesson1/Lenna.png", cv2.IMREAD_GRAYSCALE)
+
+
+# img[:20,:] = 0
+# img[-20:,:] = 255
+
+img[:, :20] = 0
+img[:, -20:] = 0
+cv2.imshow("img", img )
+cv2.waitKey(0)
+
+
