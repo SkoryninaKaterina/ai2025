7a7c247d7b22a0c848727bbe5277b89c512eab72
SkoryninaKaterina <skorynina.katerina95@gmail.com>
2025-05-15 17:57:53 +0300

    dz 19str

--- import.py
+++ import.py
@@ -3,16 +3,40 @@ import cv2
 # Завдання 3
 # Відкрийте зображення data/lesson1/Lenna.png
 # Створіть наступні зображення:
-
-img = cv2.imread("data/lesson1/Lenna.png", cv2.IMREAD_GRAYSCALE)
-
-
+# #
+# img = cv2.imread("data/lesson1/Lenna.png", cv2.IMREAD_GRAYSCALE)
+# #
+# #
 # img[:20,:] = 0
 # img[-20:,:] = 255
+# #
+# img[:, :20] = 0
+# img[:, -20:] = 0
+# cv2.imshow("img", img )
+# cv2.waitKey(0)
 
-img[:, :20] = 0
-img[:, -20:] = 0
-cv2.imshow("img", img )
+
+#DZ
+# import numpy as np
+# img = cv2.imread("data/lesson1/Lenna.png", cv2.IMREAD_GRAYSCALE)
+#
+# mask = img > 128
+# result = np.zeros_like(img)
+# result[mask] = 255
+#
+# cv2.imshow('маска > 128', result)
+# cv2.waitKey(0)
+
+
+#2
+import numpy as np
+
+img = cv2.imread("data/lesson1/baboo.jpg")
+
+result = np.zeros_like(img)
+
+result[15:45, :] = img[15:45, :]
+
+cv2.imshow("Тільки очі", result)
 cv2.waitKey(0)
-
 
